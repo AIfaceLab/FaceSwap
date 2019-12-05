8530938f0bc9d3256d1c7f02ee6964b95938828a
yanxinhao <18800206203@163.com>
2019-12-05 16:49:53 +0800

    add imagelib and utils pacekages

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+from .Logger import LogModel, LossVisualize
+from .random_utils import random_normal
+from .utils import get_image_list, get_label_images, get_label_mask, visualize_output, umeyama
+__all__ = [LogModel, LossVisualize, random_normal, get_image_list,
+           get_label_images, get_label_mask, visualize_output, umeyama]
